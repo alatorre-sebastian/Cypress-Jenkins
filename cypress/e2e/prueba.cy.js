370ed23bef4410ab2ec99e2f1e3c9a6a3329cae7
Sebastian Alatorre Lopez <alatorre.sebastian@uabc.edu.mx>
2023-09-19 14:48:30 -0700

    se modifico prueba.cy.js

--- cypress/e2e/prueba.cy.js
+++ cypress/e2e/prueba.cy.js
@@ -1,6 +1,6 @@
 describe('Prueba googleE', () => {
     it('Entra a GOOGLE y busca gatitos', () => {
         cy.visit("https://www.google.com.mx/?hl=es")
-        cy.get("#APjFqb").type("Gatitos{enter}")
+        cy.get("#APjFqb").type("Gatitos bonitos{enter}")
     });
 });
