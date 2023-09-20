2d34a59f43187d5353ebb9a1c4b04c31c9764078
Sebastian Alatorre Lopez <alatorre.sebastian@uabc.edu.mx>
2023-09-20 13:28:28 -0700

    se modifico prueba.cy.js, se agrego Deberia mostrar prueba erronea

--- cypress/e2e/prueba.cy.js
+++ cypress/e2e/prueba.cy.js
@@ -3,4 +3,10 @@ describe('Prueba googleE', () => {
         cy.visit("https://www.google.com.mx/?hl=es")
         cy.get("#APjFqb").type("Gatitos bonitos{enter}")
     });
+
+    it('Deberia mostrar prueba erronea', () => {
+        cy.visit("https://www.google.com.mx/?hl=es")
+        cy.get("#APjFqb").should('not.be.visible')
+
+    });
 });
