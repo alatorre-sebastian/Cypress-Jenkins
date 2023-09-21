d7c6ca6e4fc4c131c19f171e2594f5c3b1456fd3
Sebastian Alatorre Lopez <alatorre.sebastian@uabc.edu.mx>
2023-09-21 11:38:36 -0700

    cambios en prueba.cy.js

--- cypress/e2e/prueba.cy.js
+++ cypress/e2e/prueba.cy.js
@@ -6,7 +6,7 @@ describe('Prueba google', () => {
 
     it('Deberia mostrar prueba erronea', () => {
         cy.visit("https://www.google.com.mx/?hl=es")
-        cy.get("#APjFqb").should('be.visible')
+        cy.get("#APjFqb").should('not.be.visible')
 
     });
 });
